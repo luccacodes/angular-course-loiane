2709c0c3c45df239788915b2a561d3dbd9fe49df
Lucca Teixeira <luke.crv@gmail.com>
2024-01-17 21:33:15 -0300

    Finish lesson 64

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
-import { authGuard } from "./guards/auth.guard";
+import { AuthGuard } from "./guards/auth.guard";
 // import { CursosComponent } from "./cursos/cursos.component";
 // import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
 // import { CursoNaoEncontradoComponent } from "./cursos/curso-nao-encontrado/curso-nao-encontrado.component";
 
 const appRoutes: Routes = [
-  { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule), canActivate: [authGuard]},
-  { path: 'alunos', loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule), canActivate: [authGuard]},
-  { path: '', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
+    canActivate: [AuthGuard]},
+  { path: 'alunos',
+    loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
+    canActivate: [AuthGuard]},
+  { path: '', component: HomeComponent,
+    canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   // { path: 'curso/:id', component: CursoDetalheComponent },
   // { path: 'cursos', component: CursosComponent },
--- rotas/src/app/guards/auth.guard.ts
+++ rotas/src/app/guards/auth.guard.ts
@@ -1,19 +1,20 @@
-import { Inject, Injectable } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../login/auth.service';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-export const authGuard: CanActivateFn = (route, state) => {
-  const auth = Inject(AuthService);
-  const router = Inject(Router);
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  if (auth.usuarioEstaAutenticado) {
-    return true;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    if (this.authService.usuarioEstaAutenticado()) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
   }
-
-  router.navigate(['/login']);
-
-  return false;
-};
+}
