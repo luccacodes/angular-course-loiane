9b7a97242b235dca08b2e95b86bfa7e35184f014
Lucca Teixeira <luke.crv@gmail.com>
2024-02-28 20:28:22 -0300

    Finish lesson 85

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -7,6 +9,8 @@ import {FormsModule} from '@angular/forms';
   styleUrls: ['./template-form.component.scss']
 })
 export class TemplateFormComponent {
+  constructor(private http: HttpClient) { }
+
   usuario: any = {
     nome: null,
     email: null,
@@ -15,5 +19,18 @@ export class TemplateFormComponent {
   onSubmit(form: any) {
     console.log(form.value);
     console.log(this.usuario);
+  }
+
+  consultaCEP(cep: any) {
+    cep = cep.replace (/\D/g, '');
+
+    if (cep != null && cep !== '') {
+      let validacep = /^[0-9]{8}$/;
+      if (validacep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => console.log(dados));
+      }
+    }
   }
 }
