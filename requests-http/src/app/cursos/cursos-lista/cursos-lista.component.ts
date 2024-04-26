a3f92251c4d32cd1a0cc358276fed526ecc1106e
Lucca Teixeira <luke.crv@gmail.com>
2024-04-26 16:51:29 -0300

    Finish lesson 124

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, Subject, catchError } from 'rxjs';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,6 +14,7 @@ export class CursosListaComponent {
   // cursos: Curso[] = [];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) {}
 
@@ -21,6 +22,23 @@ export class CursosListaComponent {
     // this.service.list()
     // .subscribe(dados => this.cursos = dados);
 
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    )
+
+    this.service.list().subscribe(
+      dados => console.log(dados),
+      error => console.error(error),
+      () => console.log('Observable completo!')
+    )
   }
 }
