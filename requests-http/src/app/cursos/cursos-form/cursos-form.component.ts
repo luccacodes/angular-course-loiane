b855c0b9675a91e35f16dbd5b24b5cea61879aeb
Lucca Teixeira <luke.crv@gmail.com>
2024-05-01 12:36:28 -0300

    Finish lesson 128

--- requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CursosService } from '../cursos.service';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
 
 @Component({
   selector: 'app-cursos-form',
@@ -10,7 +12,7 @@ export class CursosFormComponent {
   form: FormGroup;
   submitted: boolean = false;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private cursosService: CursosService, private modalService: AlertModalService) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -27,6 +29,11 @@ export class CursosFormComponent {
     console.log(this.form.value);
     if(this.form.valid) {
       console.log('submit');
+      this.cursosService.create(this.form.value).subscribe(
+        success => this.modalService.showAlertSuccess('Curso criado com sucesso'),
+        error => this.modalService.showAlertDanger('Erro ao criar curso, tente novamente'),
+        () => console.log('request completo')
+      );
     }
   }
 
