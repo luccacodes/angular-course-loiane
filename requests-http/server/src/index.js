b13125b0521eee4f8e4c3b8e602ebc904ccd5176
Lucca Teixeira <luke.crv@gmail.com>
2024-05-11 14:05:20 -0300

    Finish lesson 136

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multipart = require('connect-multiparty');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+app.post('/upload', multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: 'File uploaded successfully' });
+});
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
+app.listen(8000, () => {
+  console.log('Server is running on 8000');
+});
